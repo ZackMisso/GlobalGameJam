5f1a36a97f294d44d9070f27e69b1e8c10cd2a1e
sbear260 <sbear260molten@gmail.com>
2016-01-30 10:06:27 -0600

    PICK UP SHIT
    Attach script to items that can be picked up.  Looker = player
    gameobject.

---
+++ MorningRitual/Assets/Scripts/PickUpClick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpClick : MonoBehaviour {
+
+public GameObject looker; //refers to player or camera, assumes mouse follows it
+public float distance; 
+private bool hold; 
+
+	// Use this for initialization
+	void Start () {
+		hold = false; 
+	}
+
+	void Update () {
+		if (hold == true){
+			//off is the distance times the unit vector of the player's forward
+			Vector3 off = distance*(looker.transform.forward)/(looker.transform.forward.magnitude); 
+			//pos is the new position of the object = player position pluss offset (off)
+			Vector3 pos = looker.transform.position + off; 
+			transform.position = pos; 
+		}
+	}
+
+	//When clicked
+	void OnMouseDown(){
+		if(hold == false){
+			hold = true; 
+		}
+		else{
+			hold = false; 	
+		}
+	}
+}
