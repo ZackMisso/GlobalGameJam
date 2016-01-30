5901d32b63600cbb6c2157e087e4b9daed3a2a66
dvrajito <dvrajito@sbcglobal.net>
2016-01-30 13:59:29 -0600

    Added RitualManager script

---
+++ MorningRitual/Assets/Scripts/RitualManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualManager : MonoBehaviour {
+
+    public enum RitualStage
+    {
+        start, bathroom, shower, getDressed, breakfast, coffee, leave, done
+    }
+
+    public RitualStage myStage;
+    float startDelay;
+
+    public GameObject toilet, showerKnob, showerKnobPlace, closet;
+
+	// Use this for initialization
+	void Start () {
+        myStage = RitualStage.start;
+        startDelay = 5f;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if (myStage == RitualStage.start)
+        {
+            startDelay -= Time.deltaTime;
+            if (startDelay <= 0)
+                myStage = RitualStage.bathroom;
+        }
+	}
+}
