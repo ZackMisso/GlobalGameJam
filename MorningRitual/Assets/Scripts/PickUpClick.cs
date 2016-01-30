98a06fed051a47b168a3340aaccf2a452cae9e20
sbear260 <sbear260molten@gmail.com>
2016-01-30 12:59:24 -0600

    Update PickUpClick and add PlaceItem
    make the hold bool on PickUpClick public, made forward of picked-up item
    match the player's, and created easily customizable PlaceItem script
    (for placing picked-up items).

--- MorningRitual/Assets/Scripts/PickUpClick.cs
+++ MorningRitual/Assets/Scripts/PickUpClick.cs
@@ -5,7 +5,7 @@ public class PickUpClick : MonoBehaviour {
 
 public GameObject looker; //refers to player or camera, assumes mouse follows it
 public float distance; 
-private bool hold; 
+public bool hold; 
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +19,8 @@ private bool hold;
 			//pos is the new position of the object = player position pluss offset (off)
 			Vector3 pos = looker.transform.position + off; 
 			transform.position = pos; 
+			//so the object rotates with you
+			transform.forward = looker.transform.forward; 
 			if (Input.GetMouseButtonDown(1)){
 				hold = false; 
 			}
@@ -31,4 +33,5 @@ private bool hold;
 			hold = true; 
 		}
 	}
+
 }
