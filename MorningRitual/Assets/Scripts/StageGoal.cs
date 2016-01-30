9bb51a51962bb8c4a29ed6c0c2209695ca0c1622
ZackMisso <zack441@mac.com>
2016-01-30 15:47:29 -0600

    Fixed Errors

--- MorningRitual/Assets/Scripts/StageGoal.cs
+++ MorningRitual/Assets/Scripts/StageGoal.cs
@@ -16,7 +16,7 @@ public class StageGoal : MonoBehaviour {
 
   public void Update() {
     if(walkGoal) {
-      SphericalCollider collider = keyObject.GetComponent<SphericalCollider>();
+      SphereCollider collider = keyObject.GetComponent<SphereCollider>();
       Vector3 position = transform.position;
       Vector3 cent = collider.center;
       float radius = collider.radius;
@@ -25,32 +25,6 @@ public class StageGoal : MonoBehaviour {
         // Handle stage Transition maybe
         manager.NextStage();
       }
-    }
-
-    // These two should be handled elsewhere
-
-    //else if(dropGoal) {
-    //  // do stuffs
-    //}
-    //else if(clickGoal) { // Needs to be tested
-      // does nothing... blah handled by Clickable
-
-      // Rays dont work well
-      /*RaycastHit hit;
-      float centerX = Screen.width / 2;
-      float centerY = Screen.height / 2;
-      Ray ray = Camera.ScreenPointToRay(Vector3(centerX, centerY, 0));
-      if(Physics.Raycast(ray, out hit)) {
-        if(hit.collider.gameObject == keyObject) {
-          // handle Click stuffs if needed
-          manager.NextStage();
-        }
-      }*/
-
-      //if(hit.collider.tag == "clickableCube"{
-      //  //hit.collider.gameObject now refers to the
-      //  //cube under the mouse cursor if present
-      //}
     }
   }
 }
