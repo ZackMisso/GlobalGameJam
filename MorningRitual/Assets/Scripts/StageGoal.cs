5a7abadcf322f0442594ba93aa242fb410eb7241
ZackMisso <zack441@mac.com>
2016-01-30 15:41:13 -0600

    'Finished' Stage Goal. Time to test

--- MorningRitual/Assets/Scripts/StageGoal.cs
+++ MorningRitual/Assets/Scripts/StageGoal.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class StageGoal : MonoBehaviour {
+  //public GameObject player;
   public RitualManager manager;
   public GameObject keyObject;
   public PlaceItem placeGoal;
@@ -15,14 +16,23 @@ public class StageGoal : MonoBehaviour {
 
   public void Update() {
     if(walkGoal) {
-      // do stuffs
+      SphericalCollider collider = keyObject.GetComponent<SphericalCollider>();
+      Vector3 position = transform.position;
+      Vector3 cent = collider.center;
+      float radius = collider.radius;
+      float distance = (cent-position).magnitude;
+      if(radius > distance) {
+        // Handle stage Transition maybe
+        manager.NextStage();
+      }
     }
 
     // These two should be handled elsewhere
-    else if(dropGoal) {
-      // do stuffs
-    }
-    else if(clickGoal) { // Needs to be tested
+
+    //else if(dropGoal) {
+    //  // do stuffs
+    //}
+    //else if(clickGoal) { // Needs to be tested
       // does nothing... blah handled by Clickable
 
       // Rays dont work well
