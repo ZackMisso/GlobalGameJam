4ca2db290239ccf404974a8e3e6754f6d00cc04b
ZackMisso <zack441@mac.com>
2016-01-30 13:32:17 -0600

    Finalized Movement Scripts

--- MorningRitual/Assets/Scripts/FPSWalker.cs
+++ MorningRitual/Assets/Scripts/FPSWalker.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class FPSWalker : MonoBehaviour {
   public float speed = 6.0f;
-  private Vector3 moveDirection = Vector3.zero;
+
+  // Instructions to make move ::
+  // Add mesh collider to scenery
+  // Add rigid body to camera
+  // Add spherical collider to camera
+  // Add Mouse Look and FPS Walker scripts
+  // Adjust for scenery
 
   public void FixedUpdate() {
     // Moves with arrow keys
-    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
     moveDirection = transform.TransformDirection(moveDirection);
-    //Vector3 forward = transform.forward;
-    moveDirection *= speed * Time.deltaTime;
-    //this.transform.Translate(moveDirection * Time.deltaTime,Space.World);
-    this.transform.Translate(moveDirection.x,0,moveDirection.z,Space.World);
+    moveDirection *= speed;
+
+    if (GetComponent<Rigidbody>())
+      GetComponent<Rigidbody>().velocity = new Vector3(moveDirection.x,0,moveDirection.z);
   }
 }
