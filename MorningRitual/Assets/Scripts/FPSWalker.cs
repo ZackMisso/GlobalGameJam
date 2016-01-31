78274810a7f3519d402b75e2c76e985c42dceb6f
TAG13 <tag.ewj@gmail.com>
2016-01-31 11:21:21 -0600

    Footsteps + Lower walkg speed + Fabric Tweaks
    Basic Footsteps working. Also slowed down the speed of the player.

--- MorningRitual/Assets/Scripts/FPSWalker.cs
+++ MorningRitual/Assets/Scripts/FPSWalker.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class FPSWalker : MonoBehaviour {
-  public float speed = 6.0f;
+  public float speed = 2.0f;
 
   // Instructions to make move ::
   // Add mesh collider to scenery
@@ -16,6 +16,13 @@ public class FPSWalker : MonoBehaviour {
     Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
     moveDirection = transform.TransformDirection(moveDirection);
     moveDirection *= speed;
+
+		if (moveDirection.magnitude >= 0.3f) {
+			Debug.Log ("walking");
+			Fabric.EventManager.Instance.PostEvent("PlayFootsteps", gameObject);
+		} else if (moveDirection.magnitude < 0.3) {
+			Debug.Log ("not walking");
+		}
 
     if (GetComponent<Rigidbody>())
       GetComponent<Rigidbody>().velocity = new Vector3(moveDirection.x,0,moveDirection.z);
