2b54978a1963f6b26aee28350e180e356356c298
ZackMisso <zack441@mac.com>
2016-01-31 14:19:15 -0600

    Made sound effects happen whenever after the stage is completed

--- MorningRitual/Assets/Scripts/Clickable.cs
+++ MorningRitual/Assets/Scripts/Clickable.cs
@@ -5,15 +5,18 @@ public class Clickable : MonoBehaviour {
   public StageGoal currentGoal;
   public string audioTag;
   public bool active=false;
+  public bool done=false;
 
   public void SetStageGoal(StageGoal param) {
     currentGoal = param;
   }
 
   public void OnMouseDown() {
-    Fabric.EventManager.Instance.PostEvent(audioTag, gameObject);
+    if(done)
+      Fabric.EventManager.Instance.PostEvent(audioTag, gameObject);
     if(active) {
       active = false;
+      done = true;
       //Fabric.EventManager.Instance.PostEvent(audioTag, this);
       currentGoal.manager.NextStage();
     }
