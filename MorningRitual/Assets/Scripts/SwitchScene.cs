a08be64bd67431c55e5de65a594fab4e60731003
dvrajito <dvrajito@sbcglobal.net>
2016-01-31 14:56:17 -0600

    Added an exit scene with a replay button

---
+++ MorningRitual/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScene : MonoBehaviour {
+
+    public void ClickReplay()
+    {
+        Application.LoadLevel("roomtestscene_julian");
+    }
+    
+}
