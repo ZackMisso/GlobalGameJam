1b22fadceb974eadfd50d7013f6960dd46fae1d9
ZackMisso <zack441@mac.com>
2016-01-30 18:20:47 -0600

    Added Distance Check for Picking Up Items

--- MorningRitual/Assets/Scripts/PickUpClick.cs
+++ MorningRitual/Assets/Scripts/PickUpClick.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PickUpClick : MonoBehaviour {
 
     public GameObject looker; //refers to player or camera, assumes mouse follows it
-    public float distance; 
+    public float distance;
     public bool hold;
     public bool active;
 
@@ -17,23 +17,30 @@ public class PickUpClick : MonoBehaviour {
 	void Update () {
 		if (hold == true){
 			//off is the distance times the unit vector of the player's forward
-			Vector3 off = distance*(looker.transform.forward)/(looker.transform.forward.magnitude); 
+			Vector3 off = distance*(looker.transform.forward)/(looker.transform.forward.magnitude);
 			//pos is the new position of the object = player position pluss offset (off)
-			Vector3 pos = looker.transform.position + off; 
-			transform.position = pos; 
+			Vector3 pos = looker.transform.position + off;
+			transform.position = pos;
 			//so the object rotates with you
-			transform.forward = looker.transform.forward; 
+			transform.forward = looker.transform.forward;
 			if (Input.GetMouseButtonDown(1)){
-				hold = false; 
+				hold = false;
 			}
 		}
 	}
 
-	//When clicked
-	void OnMouseDown(){
-		if(active && hold == false){
-			hold = true; 
-		}
+  // When Clicked
+  void OnMouseDown() {
+		if(checkDistance())
+			if(hold == false)
+				hold = true;
+	}
+
+	private bool checkDistance() {
+		Vector3 position = transform.position;
+		Vector3 otherPosition = looker.transform.position;
+		float dist = (otherPosition-position).magnitude;
+		return dist < minDistance;
 	}
 
 }
