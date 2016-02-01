5d754b256cb42e7b41d28eefcb5e583427925986
dvrajito <dvrajito@sbcglobal.net>
2016-01-31 22:28:52 -0600

    Added a scene to fix what wasn't working for me.

--- MorningRitual/Assets/Scripts/SwitchScene.cs
+++ MorningRitual/Assets/Scripts/SwitchScene.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class SwitchScene : MonoBehaviour {
 
+    // replay for original julien scene
     public void ClickReplay()
     {
-        Application.LoadLevel("roomtestscene_julian");
+        Application.LoadLevel("roomtestscene_julien");
     }
-    
+
+    // replay for new scene
+    public void ClickReplay1()
+    {
+        Application.LoadLevel("dana_scene");
+    }
+
 }
